d681f391da3ae9156b1128749a51721c709c4679
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-02 06:35:30 -0800

    -

---
+++ .sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+flag() {
+	for f in "$@"; do
+		[[ -e ".flags/$f" ]] || return 1
+	done
+}
+if flag local; then :; else
+	npm ci
+	alias sass="npx sass"
+fi
+for SCSS in page/*.scss; do
+	CSS="${SCSS#page/}"
+	CSS="${CSS%.scss}.css"
+	node -e "console.log(require(\"sass\").compileString(\`$(cat "$SCSS")\`).css)" > $CSS
+done
+for _PUG in page/_*.pug; do
+	HTML=${_PUG#page/_}
+	HTML=${HTML%.pug}.html
+	PUG=page/${_PUG#page/_}
+	cat << EOF > $PUG
+$(cat "_.pug")
+$(cat $_PUG)
+EOF
+	node -e "console.log(require('pug').renderFile('$PUG').normalize('NFD'))" > $HTML
+done
