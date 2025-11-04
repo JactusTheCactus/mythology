3217b734ac15fe101357321567e123ede4ebe925
JactusTheCactus <threateninglyamusing@gmail.com>
2025-11-04 04:58:52 -0800

    Update .sh

--- .sh
+++ .sh
@@ -4,15 +4,9 @@ flag() {for f in "$@"; do [[ -e ".flags/$f" ]] || return 1; done} if flag local;
 rm -rf dist/*
 mkdir -p dist
 for SCSS in src/*.scss; do
-	CSS="$SCSS"
-	CSS="${CSS#src/}"
-	CSS="${CSS%.scss}"
-	CSS="dist/$CSS.css"
-	node -e "console.log(require('sass').compile('$SCSS').css)" > "$CSS"
+	node -e "console.log(require('sass').compile('$SCSS').css)" > "dist/$(basename "$SCSS" .scss).css"
 done
 for _PUG in src/_*.pug; do
-	HTML="${_PUG#src/_}"
-	HTML="${HTML%.pug}.html"
 	PUG="dist/${_PUG#src/_}"
 	cat << EOF > "$PUG"
 include _.pug
@@ -21,6 +15,5 @@ include _head.pug $(head -n 1 "$_PUG")
 		h1 $(cat "$_PUG")
 include _foot.pug
 EOF
-	node -e "console.log(require('pug').renderFile('$PUG').normalize('NFD'))" > "$HTML"
-	rm "$PUG"
+	node -e "console.log(require('pug').renderFile('$PUG').normalize('NFD'))" > "dist/$(basename "${_PUG#_}" .pug).html"
 done
