4aa4a78e4ac4ce2b27bbbd202fb2a04ed1b277a6
JactusTheCactus <threateninglyamusing@gmail.com>
2025-11-04 05:09:10 -0800

    Update .sh

--- .sh
+++ .sh
@@ -3,9 +3,7 @@ set -euo pipefail
 flag() {for f in "$@"; do [[ -e ".flags/$f" ]] || return 1; done} if flag local; then :; else npm ci; fi
 rm -rf dist/*
 mkdir -p dist
-for SCSS in src/*.scss; do
-	node -e "console.log(require('sass').compile('$SCSS').css)" > "dist/$(basename "$SCSS" .scss).css"
-done
+npx sass src:dist --no-source-map
 for _PUG in src/_*.pug; do
 	PUG="dist/${_PUG#src/_}"
 	cat << EOF > "$PUG"
