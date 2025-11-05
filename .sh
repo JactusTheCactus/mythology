82db783433b7536367d1c51d74df882438f5c33b
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-04 17:18:17 -0800

    -

--- .sh
+++ .sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
-flag() {for f in "$@"; do [[ -e ".flags/$f" ]] || return 1; done} if flag local; then :; else npm ci; fi
+flag() {
+	for f in "$@"; do
+		[[ -e ".flags/$f" ]] || return 1
+	done
+}
+if flag local; then
+	:
+else
+	npm ci
+fi
 rm -rf dist/*
 mkdir -p dist
 npx sass src:dist --no-source-map
 for PUG in src/*.pug; do
-	node -e "console.log(require('pug').renderFile('dist/${PUG#src/_}').normalize('NFD'))" > "dist/$(basename "${PUG#_}" .pug).html"
+	node -e "console.log(require('pug').renderFile('"$PUG"').normalize('NFD'))" > "dist/$(basename "$PUG" .pug).html"
 done
